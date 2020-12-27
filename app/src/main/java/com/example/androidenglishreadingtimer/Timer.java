fb9fcdae34b08c6d7e92ba484bd8c68434c3c085
Elad Levi <60574244+elad3vii13@users.noreply.github.com>
2020-12-27 18:51:49 +0200

    more quotes
    ...

--- app/src/main/java/com/example/androidenglishreadingtimer/Timer.java
+++ app/src/main/java/com/example/androidenglishreadingtimer/Timer.java
@@ -45,10 +45,9 @@ public class Timer extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        String[] quoteArray = new String[] {"“Once you learn to read, you will be forever free.” — Frederick Douglass", "“The more that you read, the more things you will know. The more you learn, the more places you’ll go.”— Dr. Seuss, “I Can Read With My Eyes Shut!”", "“Today a reader, tomorrow a leader.” – Margaret Fuller", "“There are worse crimes than burning books.  One of them is not reading them.” – Ray Bradbury", "“Reading is to the mind what exercise is to the body.” – Richard Steele”", "“Reading is a discount ticket to everywhere.”  – Mary Schmich", "“Reading is to the mind what exercise is to the body – Joseph Addison"};
-
+        String[] quoteArray = new String[] {"“Today a reader, tomorrow a leader.” – Margaret Fuller", "“A word after a word after a word is power.” – Margaret Atwood", "“Show me a family of readers, and I will show you the people who move the world.” – Napoleon Bonaparte", "“A book is a garden, an orchard, a storehouse, a party, a company by the way, a counselor, a multitude of counselors.” – Charles Baudelaire", "“If I were a young person today, trying to gain a sense of myself in the world, I would do that by reading” – Maya Angelou", "“Reading should not be presented to children as a chore, a duty. It should be offered as a gift.” – Kate DiCamillo", "“I think books are like people, in the sense that they’ll turn up in your life when you most need them.” – Emma Thompson", "“Books are a uniquely portable magic.” – Stephen King", "“Books are mirrors: You only see in them what you already have inside you.” – Carlos Ruiz Zafón", "Think before you speak. Read before you think. – Fran Lebowitz", "“If you don’t like to read, you haven’t found the right book.” – J.K. Rowling", "“I can feel infinitely alive curled up on the sofa reading a book.” – Benedict Cumberbatch", "“Some books leave us free and some books make us free.” – Ralph Waldo Emerson", "“Writing and reading decrease our sense of isolation. They feed the soul.” – Anne Lamott", "“Books and doors are the same thing. You open them, and you go through into another world.” – Jeanette Winterson", "“Books are, let’s face it, better than everything else.” – Nick Hornby", "We read to know we are not alone. – C.S. Lewis", "“Read a lot. Expect something big from a book.” – Susan Sontag", "“Once you learn to read, you will be forever free.” – Frederick Douglass", "“Books save lives.” – Laurie Anderson", "A room without books is like a body without a soul. – Cicero", "“The reading of all good books is like a conversation with the finest minds of past centuries.” – Rene Descartes", "“That’s the thing about books. They let you travel without moving your feet.” – Jhumpa Lahiri", "“I love the way that each book — any book — is its own journey. You open it, and off you go…” – Sharon Creech", "“Reading is an exercise in empathy; an exercise in walking in someone else’s shoes for a while.” – Malorie Blackman", "“Reading is escape.” – Nora Ephron", "“Reading is important. If you know how to read, then the whole world opens up to you.” – Barack Obama", "“Books may well be the only true magic.” – Alice Hoffman", "The more that you read, the more things you will know. The more that you learn, the more places you’ll go. —Dr. Seuss", "Reading is a discount ticket to everywhere. —Mary Schmich", "A book is a dream you hold in your hands. —Neil Gaiman", "Reading is an active, imaginative act; it takes work. —Khaled Hosseini", "Reading is departure and arrival. —Terri Guillemets", " Books are the plane, and the train, and the road. They are the destination, and the journey. They are home. —Anna Quindlen"};
         Random random = new Random();
-        int RANDOM_QUATE_NUMBER = random.nextInt(7);
+        int RANDOM_QUATE_NUMBER = random.nextInt(quoteArray.length-1);
         quote_tv = findViewById(R.id.quote);
         quote_tv.setText(quoteArray[RANDOM_QUATE_NUMBER]);
 
@@ -230,15 +229,15 @@ public class Timer extends AppCompatActivity {
             }
         });
 
-        alert.setNegativeButton("Cancel",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // TODO Auto-generated method stub
-                        BookName = "Default Value";
-                        afterPopup();
-                        return;
-                    }
-                });
+//        alert.setNegativeButton("Cancel",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        // TODO Auto-generated method stub
+//                        BookName = "Default Value";
+//                        afterPopup();
+//                        return;
+//                    }
+//                });
         alert.show();
     }
 
